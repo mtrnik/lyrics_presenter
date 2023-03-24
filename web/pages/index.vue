af420022a0f387c29f4e58b6fcf65a24dacf35c4
Martin Trnik <martin@oktodigital.com>
2023-03-24 15:26:06 +0100

    [web] - lyrics wired to presentation

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -2,27 +2,23 @@
     <v-app id="inspire">
 
         <v-navigation-drawer v-model="drawer">
-<!--            <v-sheet-->
-<!--                color="grey-lighten-4"-->
-<!--                class="pa-4"-->
-<!--            >-->
+            <v-sheet
+                color="grey-lighten-4"
+                class="pa-4"
+            >
 <!--                <v-avatar-->
 <!--                    class="mb-4"-->
 <!--                    color="grey-darken-1"-->
 <!--                    size="64"-->
 <!--                ></v-avatar>-->
 
-<!--                <div>john@google.com</div>-->
-<!--            </v-sheet>-->
+                <div @click="openPresenter">Present</div>
+            </v-sheet>
 
-<!--            <v-divider></v-divider>-->
+            <v-divider></v-divider>
 
             <v-list>
-                <v-list-item
-                    v-for="[icon, text] in links"
-                    :key="icon"
-                    link
-                >
+                <v-list-item v-for="[icon, text] in links" :key="icon" link >
                     <template v-slot:prepend>
                         <v-icon>{{ icon }}</v-icon>
                     </template>
@@ -33,30 +29,18 @@
         </v-navigation-drawer>
 
         <v-main>
-            <v-container
-                class="py-8 px-6"
-                fluid
-            >
+            <v-container class="py-8 px-6" fluid>
                 <v-row>
-                    <v-col
-                        v-for="card in cards"
-                        :key="card"
-                        cols="12"
-                    >
+                    <v-col v-for="song in songs" :key="song.id" cols="12">
                         <v-card>
-
-                            <v-list lines="two">
-                                <v-list-subheader>{{ card }}</v-list-subheader>
-                                <template v-for="n in 6" :key="n">
-                                    <v-list-item>
-                                        <template v-slot:prepend>
-                                            <v-avatar color="grey-darken-1"></v-avatar>
-                                        </template>
-
-                                        <v-list-item-title>Message {{ n }}</v-list-item-title>
+                            <v-list>
+                                <v-list-subheader>{{ song.name }}</v-list-subheader>
+                                <template v-for="(verse, index) in song.verses" :key="index">
+                                    <v-list-item @click="updateLocalStorage( verse.lyrics )">
+                                        <v-list-item-title>{{ verse.name }}</v-list-item-title>
 
                                         <v-list-item-subtitle>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                                            {{ verse.lyrics }}
                                         </v-list-item-subtitle>
                                     </v-list-item>
 
@@ -91,13 +75,34 @@ export default defineNuxtComponent({
     name: "index",
     data() {
         return {
-            cards: ['Today', 'Yesterday'],
+            songs: [
+                {
+                    id: 1,
+                    name: 'Song 1',
+                    verses: [
+                        { name: 'Sloha 1', lyrics: 'Song 1, Verse 1' },
+                        { name: 'Sloha 2', lyrics: 'Song 1, Verse 2' },
+                        { name: 'Refrén', lyrics: 'Song 1, Verse 3' },
+                        { name: 'Bridge', lyrics: 'Song 1, Verse 4' },
+                    ]
+                },
+                {
+                    id: 2,
+                    name: 'Song 2',
+                    verses: [
+                        { name: '', lyrics: 'Song 2, Verse 1' },
+                        { name: '', lyrics: 'Song 2, Verse 2' },
+                        { name: '', lyrics: 'Song 2, Verse 3' },
+                        { name: '', lyrics: 'Song 2, Verse 4' },
+                    ]
+                }
+            ],
             drawer: null,
             links: [
-                ['mdi-inbox-arrow-down', 'Live'],
-                ['mdi-send', 'Send'],
-                ['mdi-delete', 'Trash'],
-                ['mdi-alert-octagon', 'Spam'],
+                ['mdi-play-box', 'Live'],
+                ['mdi-clipboard-text-play-outline', 'Songs'],
+                // ['mdi-delete', 'Trash'],
+                // ['mdi-alert-octagon', 'Spam'],
             ],
         }
     },
@@ -105,8 +110,8 @@ export default defineNuxtComponent({
         openPresenter() {
             window.open( window.location.href + 'presenter' , "_blank", "location=yes,height=570,width=520,scrollbars=yes,status=yes");
         },
-        updateLocalStorage(index: number) {
-            localStorage.setItem('lyrics_presenter_verse', JSON.stringify({ lyrics: 'verse' + index + 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, eius fuga! Aspernatur cupiditate delectus dolorem earum esse ipsum, iusto molestiae molestias nemo non obcaecati quisquam quod similique sunt tempore voluptatibus.' } ) )
+        updateLocalStorage( lyrics: string ) {
+            localStorage.setItem('lyrics_presenter_verse', JSON.stringify({ lyrics } ) )
         }
     }
 })
