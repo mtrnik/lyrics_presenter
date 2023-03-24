2d7ad7a32f244bc112d1451abd236d519ab4a8e6
Martin Trnik <martin@oktodigital.com>
2023-03-24 15:07:13 +0100

    [web] - vuetify installed

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
+    css: [
+        '~/assets/css/main.css',
+        'vuetify/lib/styles/main.sass',
+        '@mdi/font/css/materialdesignicons.min.css'
+    ],
+    build: {
+        transpile: ['vuetify'],
+    },
     postcss: {
         plugins: {
             tailwindcss: {},
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,20 +1,106 @@
 <template>
-    <div>
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-        <span @click="openPresenter">Open Presenter</span>
-        <ul>
-            <li v-for="index in [0,1,2,3,4,5]" :key="index" @click="updateLocalStorage(index)">
-                verse {{ index }}
-            </li>
-        </ul>
-    </div>
+    <v-app id="inspire">
+
+        <v-navigation-drawer v-model="drawer">
+<!--            <v-sheet-->
+<!--                color="grey-lighten-4"-->
+<!--                class="pa-4"-->
+<!--            >-->
+<!--                <v-avatar-->
+<!--                    class="mb-4"-->
+<!--                    color="grey-darken-1"-->
+<!--                    size="64"-->
+<!--                ></v-avatar>-->
+
+<!--                <div>john@google.com</div>-->
+<!--            </v-sheet>-->
+
+<!--            <v-divider></v-divider>-->
+
+            <v-list>
+                <v-list-item
+                    v-for="[icon, text] in links"
+                    :key="icon"
+                    link
+                >
+                    <template v-slot:prepend>
+                        <v-icon>{{ icon }}</v-icon>
+                    </template>
+
+                    <v-list-item-title>{{ text }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-main>
+            <v-container
+                class="py-8 px-6"
+                fluid
+            >
+                <v-row>
+                    <v-col
+                        v-for="card in cards"
+                        :key="card"
+                        cols="12"
+                    >
+                        <v-card>
+
+                            <v-list lines="two">
+                                <v-list-subheader>{{ card }}</v-list-subheader>
+                                <template v-for="n in 6" :key="n">
+                                    <v-list-item>
+                                        <template v-slot:prepend>
+                                            <v-avatar color="grey-darken-1"></v-avatar>
+                                        </template>
+
+                                        <v-list-item-title>Message {{ n }}</v-list-item-title>
+
+                                        <v-list-item-subtitle>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                                        </v-list-item-subtitle>
+                                    </v-list-item>
+
+                                    <v-divider
+                                        v-if="n !== 6"
+                                        :key="`divider-${n}`"
+                                        inset
+                                    ></v-divider>
+                                </template>
+                            </v-list>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+<!--    <div>-->
+<!--        <h1 class="text-3xl font-bold underline">-->
+<!--            Hello world!-->
+<!--        </h1>-->
+<!--        <span @click="openPresenter">Open Presenter</span>-->
+<!--        <ul>-->
+<!--            <li v-for="index in [0,1,2,3,4,5]" :key="index" @click="updateLocalStorage(index)">-->
+<!--                verse {{ index }}-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--    </div>-->
 </template>
 
 <script lang="ts">
 export default defineNuxtComponent({
     name: "index",
+    data() {
+        return {
+            cards: ['Today', 'Yesterday'],
+            drawer: null,
+            links: [
+                ['mdi-inbox-arrow-down', 'Live'],
+                ['mdi-send', 'Send'],
+                ['mdi-delete', 'Trash'],
+                ['mdi-alert-octagon', 'Spam'],
+            ],
+        }
+    },
     methods: {
         openPresenter() {
             window.open( window.location.href + 'presenter' , "_blank", "location=yes,height=570,width=520,scrollbars=yes,status=yes");
