35009aa320640773c019938f4ed9aa75972c1e83
Martin Trnik <martin@oktodigital.com>
2023-05-05 08:34:51 +0200

    [electron-app] - added vue3 desktop application

---
+++ electron-app/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Hello, Electron!</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'App'
+})
+</script>
+
+<style>
+div {
+    font-size: 24px;
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
